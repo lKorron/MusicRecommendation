52d4523f53fb96c387270df5fc6bbb624a51d7fa
Korron <lkorron@yandex.ru>
2024-04-24 13:14:40 +0300

    trainer done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from transformers import AutoTokenizer, AutoModel, DataCollatorWithPadding, TrainingArguments, Trainer, T5ForQuestionAnswering, T5Tokenizer, T5ForConditionalGeneration
-from peft import PeftModel, PeftConfig, get_peft_model, LoraConfig
+from transformers import DataCollatorWithPadding, TrainingArguments, Trainer, T5Tokenizer, T5ForConditionalGeneration
+from peft import get_peft_model, LoraConfig
 from datasets import load_dataset, DatasetDict
 
 dataset = load_dataset("csv", data_files="rephrased_questions.csv")
@@ -30,19 +30,19 @@ task_prefix = "answer"
 
 def tokenize_function(example):
     tokenized_data = tokenizer(
-        example["Question"], text_target=example["Answer"], truncation=True, padding='max_length', max_length=512)
+        example["Question"], text_target=example["Answer"], truncation=True, padding='max_length', max_length=128)
     return tokenized_data
 
 
 tokenized_dataset = dataset.map(tokenize_function, batched=True)
 
-peft_config = LoraConfig(task_type="SEQ_2_SEQ_LM", r=2,
+peft_config = LoraConfig(task_type="SEQ_2_SEQ_LM", r=1,
                          lora_alpha=32, target_modules=["q", "v"], lora_dropout=0.01)
 
 lora_model = get_peft_model(model, peft_config)
 
 lr = 3e-4
-batch_size = 2
+batch_size = 1
 num_epochs = 2
 
 # training_args = TrainingArguments(
@@ -60,8 +60,13 @@ num_epochs = 2
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 
-training_args = TrainingArguments("test-trainer")
-trainer = Trainer(model, training_args, train_dataset=tokenized_dataset["train"],
+training_args = TrainingArguments(
+    "test-trainer", per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=num_epochs)
+
+
+trainer = Trainer(lora_model, training_args, train_dataset=tokenized_dataset["train"],
                   eval_dataset=tokenized_dataset["valid"],
                   data_collator=data_collator, tokenizer=tokenizer)
 trainer.train()
