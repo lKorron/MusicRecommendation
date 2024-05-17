11bba920eb89e232ae863a6de8a0a4eb199b7df6
Korron <lkorron@yandex.ru>
2024-05-17 14:44:47 +0300

    qlora fine-tuning

---
+++ inference.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+from datasets import load_dataset
+import transformers
+
+
+def get_bot_response(request, model, tokenizer):
+
+
+    model.eval() # model in evaluation mode (dropout modules are deactivated)
+
+
+
+    # craft prompt
+    intstructions_string = f"""MusicBot, functioning as a virtual bot that recommend music composition to user. Give the user the name of the song, year, album and artist.\
+    It ends response with its signature '–MusicBot'. \
+    
+    Please respond to the following request and give the recommendation:
+    """
+
+    prompt_template = lambda comment: f'''[INST] {intstructions_string} \n{comment} \n[/INST]'''
+    prompt = prompt_template(request)
+
+    print(prompt)
+    # tokenize input
+    inputs = tokenizer(prompt, return_tensors="pt")
+
+    # generate output
+    outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"), max_new_tokens=140)
+
+    print(tokenizer.batch_decode(outputs)[0])
--- main.py
+++ main.py
@@ -1,106 +1,122 @@
 import torch
-from transformers import DataCollatorWithPadding, TrainingArguments, Trainer, T5Tokenizer, T5ForConditionalGeneration
-from peft import get_peft_model, LoraConfig
+import transformers
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
 from datasets import load_dataset, DatasetDict
-
 from config import config
+from inference import get_bot_response
 
-dataset = load_dataset("csv", data_files="rephrased_questions.csv")
+dataset = load_dataset("csv", data_files="music_bot_dataset.csv")
 
-train_testvalid = dataset['train'].train_test_split(test_size=0.2)
-test_valid = train_testvalid['test'].train_test_split(test_size=0.5)
+train_test_valid = dataset['train'].train_test_split(test_size=0.2)
+test_valid = train_test_valid['test'].train_test_split(test_size=0.5)
 
 
 dataset = DatasetDict({
-    'train': train_testvalid['train'],
+    'train': train_test_valid['train'],
     'test': test_valid['test'],
     'valid': test_valid['train']
 })
 
 
-model = T5ForConditionalGeneration.from_pretrained("t5-base")
-tokenizer = T5Tokenizer.from_pretrained("t5-base")
+model_name = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
+
+model = AutoModelForCausalLM.from_pretrained(model_name,
+                                             device_map="auto",
+                                             trust_remote_code=False,
+                                             revision="main")
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+# prepare data
+def tokenize_function(examples):
+    text = examples["sample"]
 
 
-if tokenizer.pad_token is None:
-    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+    tokenizer.truncation_side = "left"
+    tokenized_inputs = tokenizer(
+        text,
+        return_tensors="np",
+        truncation=True,
+        max_length=512
+    )
+
+    return tokenized_inputs
+
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+# pad token should be null?
 
 if tokenizer.eos_token is None:
     tokenizer.add_special_tokens({"eos_token": "</s>"})
 
-task_prefix = "answer"
+tokenizer.pad_token = tokenizer.eos_token
+
+data_collator = transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
+
+#prepare model
+
+model.train()
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+config = LoraConfig(
+    r=8,
+    lora_alpha=32,
+    target_modules=["q_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, config)
+model.print_trainable_parameters()
 
 
-# def tokenize_function(example):
-#     tokenized_data = tokenizer(
-#         example["Question"], text_target=example["Answer"], truncation=True, padding='max_length', max_length=128)
-#     return tokenized_data
+lr = 2e-4
+batch_size = 32
+num_epochs = 5
 
-def tokenize_function(examples):
-    input_text = ["question: " + q for q,
-                  a in zip(examples['Question'], examples['Answer'])]
-    model_inputs = tokenizer(input_text, max_length=128, truncation=True,
-                             padding="max_length", add_special_tokens=True)
-
-    labels = tokenizer(examples['Answer'], max_length=128,
-                       truncation=True, padding="max_length").input_ids
-    labels_with_ignore_index = [[(label if label != tokenizer.pad_token_id else -100)
-                                 for label in label_seq] for label_seq in labels]
-
-    model_inputs["labels"] = labels_with_ignore_index
-
-    return model_inputs
-
-
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
-
-peft_config = LoraConfig(task_type="SEQ_2_SEQ_LM", r=config["r"],
-                         lora_alpha=32, target_modules=["q", "v"], lora_dropout=0.01)
-
-lora_model = get_peft_model(model, peft_config)
-
-# этот lr пока не используется в модели, потом будем гиперпараметры настраивать
-lr = config["lr"]
-batch_size = config["batch_size"]
-num_epochs = config["num_epochs"]
-
-# training_args = TrainingArguments(
-#     output_dir="/Users/grigorijnikitin/.cache/huggingface/hub/preds",
-#     learning_rate=lr,
-#     per_device_train_batch_size=batch_size,
-#     per_device_eval_batch_size=batch_size,
-#     num_train_epochs=num_epochs,
-#     weight_decay=0.01,
-#     evaluation_strategy="no",
-#     save_strategy="no",
-#     load_best_model_at_end=True)
-
-
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-
-training_args = TrainingArguments(
-    "test-trainer", per_device_train_batch_size=batch_size,
+# define training arguments
+training_args = transformers.TrainingArguments(
+    output_dir="MusicBot-ft",
+    learning_rate=lr,
+    per_device_train_batch_size=batch_size,
     per_device_eval_batch_size=batch_size,
-    num_train_epochs=num_epochs, evaluation_strategy="steps", logging_steps=config["logging_steps"], logging_strategy="steps")
+    num_train_epochs=num_epochs,
+    weight_decay=0.01,
+    logging_strategy="epoch",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    gradient_accumulation_steps=4,
+    warmup_steps=2,
+    fp16=True,
+    optim="paged_adamw_8bit",
+)
 
 
-trainer = Trainer(lora_model, training_args, train_dataset=tokenized_dataset["train"],
-                  eval_dataset=tokenized_dataset["valid"],
-                  data_collator=data_collator, tokenizer=tokenizer)
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
+    args=training_args,
+    data_collator=data_collator
+)
+
+model.config.use_cache = False
+
 trainer.train()
 
-device = torch.device(
-    "cuda") if torch.cuda.is_available() else torch.device("cpu")
+trainer.save_model("tuned_model")
 
-question = "What's a good song that Get ready to headbang to the ultimate mix of blues rock, punk, rock n roll, alternative, blues, Canadian, metal, rock, and hard rock in this electrifying song that's sure to leave you craving for more!.?"
-
-input_ids = tokenizer(
-    question, return_tensors="pt"
-).input_ids
-output = lora_model.generate(input_ids=input_ids.to(device))
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+# model = AutoModelForCausalLM.from_pretrained("MusicBot-ft/checkpoint-250",
+#                                              device_map="auto",
+#                                              trust_remote_code=False,
+#                                              revision="main")
 
 
-if input("Save model? y/n") == 'y':
-    trainer.save_model("./models/model")
+get_bot_response("Give me music for dreaming", model, tokenizer)
